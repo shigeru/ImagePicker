bc4dff69e7e52a89493badb6d0ba0a4f6af7397d
Shigeru Nakaya <shigeru.nakaya@gmail.com>
2021-11-13 11:32:04 +0900

    Fixed a bug that caused an error if the folder name could not be retrieved.

--- imagepicker/src/main/java/com/nguyenhoanglam/imagepicker/ui/imagepicker/ImagePickerViewModel.kt
+++ imagepicker/src/main/java/com/nguyenhoanglam/imagepicker/ui/imagepicker/ImagePickerViewModel.kt
@@ -89,7 +89,7 @@ class ImagePickerViewModel(application: Application) : AndroidViewModel(applicat
                     val id = cursor.getLong(idColumn)
                     val name = cursor.getString(nameColumn)
                     val bucketId = cursor.getLong(bucketIdColumn)
-                    val bucketName = cursor.getString(bucketNameColumn)
+                    val bucketName = cursor.getString(bucketNameColumn) ?: "$bucketId"
 
                     val uri = ContentUris.withAppendedId(imageCollectionUri, id)
 
